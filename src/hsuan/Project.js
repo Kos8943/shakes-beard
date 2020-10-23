d3de96979425f263dcff382c4eeedecb3646f217
yuhsuan94120 <69293709+yuhsuan94120@users.noreply.github.com>
2020-10-23 14:25:46 +0800

    1023-1

--- src/hsuan/Project.js
+++ src/hsuan/Project.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Project.css";
+import search from './icon/search.svg';
 import {
   Container,
   Row,
@@ -25,67 +26,93 @@ function Project() {
             aria-describedby="emailHelp"
             placeholder="搜尋商品"
           ></input>
-        </div>
+          <div className="searchicon">
+           <img src={search}></img></div>
+           </div>
+        
+        <div class="col-md-3">
         <div className="sort">
           <h3>分類</h3>
-        </div>
-        <hr></hr>
-        {/* <Accordion>
-  <Card className="
-    menu">
-    <Card.Header className="">
-      <Accordion.Toggle as={Button} variant="link" eventKey="0">
-        Click me!
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="0">
-      <Card.Body>Hello! I'm the body</Card.Body>
-    </Accordion.Collapse>
-  </Card>
-  <Card className="
-    menu">
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="1">
-        Click me!
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="1">
-      <Card.Body>Hello! I'm another body</Card.Body>
-    </Accordion.Collapse>
-  </Card>
-</Accordion> */}
+        </div></div>
+        
+
+
+    
       </Container>
-      <ul id="product-menu">
-  <li class="ui-state-disabled"><div>優惠推薦1</div></li>
-  <ul>
-      <li class="ui-state-disabled"><div>本月優惠</div></li>
-      <li><div>免運專區</div></li>
-  </ul>
-  
-  <li class="ui-state-disabled"><div>頭髮造型</div></li>
-  <ul>
-      <li class="ui-state-disabled"><div>髮蠟</div></li>
-      <li><div>梳子</div></li>
-  </ul>
-  <li class="ui-state-disabled"><div>刮鬍修容</div></li>
-  <ul>
-      <li class="ui-state-disabled"><div>刮鬍刀</div></li>
-      <li><div>刮鬍刷</div></li>
-      <li><div>直立架</div></li>
-  </ul>
-  <li class="ui-state-disabled"><div>服飾配件</div></li>
-  <ul>
-      <li class="ui-state-disabled"><div>帽子</div></li>
-      <li><div>墨鏡</div></li>
-      <li><div>領帶/領結</div></li>
-      <li><div>領帶夾</div></li>
-      <li><div>口袋巾</div></li>
-      <li><div>袖扣</div></li>
-      <li><div>皮帶</div></li>
-      <li><div>鈔票夾</div></li>
-      
-  </ul>
-</ul>
+      <div class="container ui-state-disabled">
+        <div class="col-md-3">
+          <ul id="product-menu">
+            <li class="ui-state-disabled1">
+              <div>優惠推薦</div>
+            </li>
+        
+
+            <ul>
+              <li class="ui-state-disabled">
+                <div>本月優惠</div>
+              </li>
+              <li>
+                <div>免運專區</div>
+              </li>
+            </ul>
+          
+
+          <li class="ui-state-disabled">
+            <div>頭髮造型</div>
+          </li>
+          <ul>
+            <li class="ui-state-disabled">
+              <div>髮蠟</div>
+            </li>
+            <li>
+              <div>梳子</div>
+            </li>
+          </ul>
+          <li class="ui-state-disabled">
+            <div>刮鬍修容</div>
+          </li>
+          <ul>
+            <li class="ui-state-disabled">
+              <div>刮鬍刀</div>
+            </li>
+            <li>
+              <div>刮鬍刷</div>
+            </li>
+            <li>
+              <div>直立架</div>
+            </li>
+          </ul>
+          <li class="ui-state-disabled">
+            <div>服飾配件</div>
+          </li>
+          <ul>
+            <li class="ui-state-disabled">
+              <div>帽子</div>
+            </li>
+            <li>
+              <div>墨鏡</div>
+            </li>
+            <li>
+              <div>領帶/領結</div>
+            </li>
+            <li>
+              <div>領帶夾</div>
+            </li>
+            <li>
+              <div>口袋巾</div>
+            </li>
+            <li>
+              <div>袖扣</div>
+            </li>
+            <li>
+              <div>皮帶</div>
+            </li>
+            <li>
+              <div>鈔票夾</div>
+            </li>
+          </ul>
+        </ul></div>
+      </div>
     </>
   );
 }
