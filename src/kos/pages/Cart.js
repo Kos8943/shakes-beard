57ed49f72297677b6517773fa0c2ce496b46722e
Kos8943 <a26095998@gmail.com>
2020-10-26 15:26:56 +0800

    cart step 1 完成80%

--- src/kos/pages/Cart.js
+++ src/kos/pages/Cart.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import CartCss from '../styles/CartCss.scss'
+import Delete from '../icon/delete.svg'
 
 function Cart() { 
     const abc = window.screen.width
@@ -25,25 +26,69 @@ function Cart() {
                     <div className="cartStepLine"></div>
                     <div>
                         <div className="cartStepCircle otherStep">2</div>
-                        <div className="cartStepFont1">付款方式</div>   
+                        <div className="cartStepFont">付款方式</div>   
                     </div>
                     <div className="cartStepLine"></div>
                     <div>
                         <div className="cartStepCircle otherStep">3</div>
-                        <div className="">完成訂單</div>
+                        <div className="cartStepFont">完成訂單</div>
                     </div>   
                     </div>
                 </div>
                 <div className="cartItem d-flex">
-                    <img src=""></img>
+                    <img className="itemImg" src=""></img>
                     <div className="itemName my-auto">Ritmo di vita 陀飛輪袖扣</div>
-                    <select>
+                    <select className="selectHigh">
+                        <option value="1">經典黑</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <select className="selectHigh">
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                     </select>
-
+                    <span className="unitPrice">NT$1,441</span>
+                    <span className="subtotal">NT$2,882</span>
+                    <img src="" className="deleteIcon"></img>
                 </div>
+
+                <div className="cartItem d-flex">
+                    <img className="itemImg" src=""></img>
+                    <div className="itemName my-auto">Ritmo di vita 陀飛輪袖扣</div>
+                    <select className="selectHigh">
+                        <option value="1">經典黑</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <select className="selectHigh">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <span className="unitPrice">NT$1,441</span>
+                    <span className="subtotal">NT$2,882</span>
+                    <img src="" className="deleteIcon"></img>
+                </div>
+                <div className="cartItem d-flex">
+                    <img className="itemImg" src=""></img>
+                    <div className="itemName my-auto">Ritmo di vita 陀飛輪袖扣</div>
+                    <select className="selectHigh">
+                        <option value="1">經典黑</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <select className="selectHigh">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <span className="unitPrice">NT$1,441</span>
+                    <span className="subtotal">NT$2,882</span>
+                    <img src="" className="deleteIcon"></img>
+                </div>
+
+                
             </>
         )
     }
