9730c4c5ff5fcc7e20e376ebdc9c2f34044c56e3
Ellie <s95147852@gmail.com>
2020-10-26 16:44:32 +0800

    1026

--- src/yen/LognIn.js
+++ src/yen/LognIn.js
@@ -1,9 +1,53 @@
 import React, { useState, useEffect } from 'react'
+import LognInCss from './styles/LognInCss.scss'
 
 function LognIn(props) {
    
     return (
-      <></>
+      <>
+      <div className="logninArea">111
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </>
        
     )
   
