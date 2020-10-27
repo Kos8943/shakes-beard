55ebef3b1b4890219d80cb73641e7f8c2e13a51c
Lin yu <canty38@yahoo.com>
2020-10-27 17:16:39 +0800

    new

--- src/zihyu/GiftBox.js
+++ src/zihyu/GiftBox.js
@@ -15,16 +15,18 @@ class Game extends React.Component
         <div className="blueBox">
           <div className="whiteBox">
             <div className="line-top">
-              <div className="line-under">
-                <div className="title">客製化修容禮盒</div>
-                  <div className="line-under">
-                    <div className="line-top2"></div>
-                  </div>
-              </div>
-              
+              <div className="line-under"></div>
+              <div className="title">客製化修容禮盒</div>
+              <div className="line-under"></div>
+              <div className="line-top2"></div>
+              <div className="subtitle">FOR  THE  MAN  IN  YOUR  LIFE.</div>
+              <div className="line-middle"></div>
+              <div className="thirdTitle">獨家客製化禮盒，傳遞最真摯的心意</div>
+              <div className="btn"><button className="startButton">開始製作</button></div>
             </div>
-            
           </div>
+
+          
         </div>
         
 
