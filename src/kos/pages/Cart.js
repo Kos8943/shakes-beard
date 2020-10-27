e8cd0ac2315265a6b39e70aa018c96c379ee04cd
Kos8943 <a26095998@gmail.com>
2020-10-27 15:52:41 +0800

    購物車(網頁)

--- src/kos/pages/Cart.js
+++ src/kos/pages/Cart.js
@@ -1,19 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import CartCss from '../styles/CartCss.scss'
 import Delete from '../icon/delete.svg'
+import cuf1 from '../img/1-3.jpg'
 
 function Cart() { 
-    const abc = window.screen.width
-    const itemStyle = ["經典黑", "123", "123"]
-    
-
-    if(abc < 376) {
-        return (
-            <>
-                <div>123</div>
-            </>
-        )
-    } else {
         return (
             <>
                 <div className="cartArea">
@@ -36,26 +26,26 @@ function Cart() {
                     </div>
                 </div>
                 <div>
-                    <div className="cartItem d-flex col-12">
-                        <img className="itemImg col-1" src=""></img>
-                        <div className="itemName my-auto col-3">Ritmo di vita 陀飛輪袖扣</div>
-                        <select className="selectHigh col-1">
+                    <div className="cartItem d-flex">
+                        <img className="itemImg" src={cuf1}></img>
+                        <div className="itemName my-auto">Ritmo di vita 陀飛輪袖扣</div>
+                        <select className="selectHigh">
                             <option value="1">經典黑</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                         </select>
-                        <select className="selectHigh col-1">
+                        <select className="selectHigh">
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                         </select>
-                        <span className="unitPrice col-1">NT$1,441</span>
-                        <span className="subtotal col-1">NT$2,882</span>
-                        <img src="" className="deleteIcon col-1"></img>
+                        <span className="unitPrice">NT$1,441</span>
+                        <span className="subtotal ">NT$2,882</span>
+                        <img src={Delete} className="deleteIcon"></img>
                     </div>
 
                     <div className="cartItem d-flex">
-                        <img className="itemImg" src=""></img>
+                        <img className="itemImg" src={cuf1}></img>
                         <div className="itemName my-auto">Ritmo di vita 陀飛輪袖扣</div>
                         <select className="selectHigh">
                             <option value="1">經典黑</option>
@@ -69,10 +59,10 @@ function Cart() {
                         </select>
                         <span className="unitPrice">NT$1,441</span>
                         <span className="subtotal">NT$2,882</span>
-                        <img src="" className="deleteIcon"></img>
+                        <img src={Delete} className="deleteIcon"></img>
                     </div>
                     <div className="cartItem d-flex">
-                        <img className="itemImg" src=""></img>
+                        <img className="itemImg" src={cuf1}></img>
                         <div className="itemName my-auto">Ritmo di vita 陀飛輪袖扣</div>
                         <select className="selectHigh">
                             <option value="1">經典黑</option>
@@ -86,14 +76,32 @@ function Cart() {
                         </select>
                         <span className="unitPrice">NT$1,441</span>
                         <span className="subtotal">NT$2,882</span>
-                        <img src="" className="deleteIcon"></img>
+                        <img src={Delete} className="deleteIcon"></img>
                     </div>
+                    <div className="priceArea ml-auto d-flex">
+                        <div className="priceArea font">
+                            <div>優惠代碼：</div>
+                            <div>折扣：</div>
+                            <div>運費：</div>
+                            <div>總價：</div>
+                        </div>
+
+                        <div className="priceArea font">
+                            <div><input></input></div>
+                            <div>NT$ -150</div>
+                            <div>NT$ 150</div>
+                            <div>NT$ 8,796</div>
+                        </div>
+                        
+                    </div>
+                    <div className="sumitBtn ml-auto">
+                        <button>送出訂單</button>
+                    </div>
+                    
                 </div>
                 
             </>
-        )
-    }
-    
+        )   
 }
 
 export default Cart
