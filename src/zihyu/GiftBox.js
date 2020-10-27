43fbef3c56a1351f1879f55aa09d15a5f61694b2
Lin yu <canty38@yahoo.com>
2020-10-27 14:42:22 +0800

    new

--- src/zihyu/GiftBox.js
+++ src/zihyu/GiftBox.js
@@ -11,7 +11,18 @@ class Game extends React.Component
       <div className="contain">
 
         <div className="background"></div>
-        <div className="background2"></div>
+        <div className="background_white"></div>
+        <div className="blueBox">
+          <div className="whiteBox">
+            <div className="line-top">
+              <div className="line-under"></div>
+            </div>
+            
+          </div>
+        </div>
+        
+
+        
         <div className="status"> {MyString} <br/></div>
       </div>
     );
