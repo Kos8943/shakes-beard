36cefcb0e0eae24d2c60ee36d75c68ac5dad653e
yuhsuan94120 <69293709+yuhsuan94120@users.noreply.github.com>
2020-10-29 11:30:12 +0800

    banner

--- src/hsuan/component/Banner.js
+++ src/hsuan/component/Banner.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import original from '../icon/original.jpg';
-import { Carousel } from 'react-bootstrap'
+import React from "react";
+import original from "../icon/original.jpg";
+import { Carousel } from "react-bootstrap";
 // import background from './card01.jpg'
 
 function Banner(props) {
-  console.log('Banner', props)
+  console.log("Banner", props);
   return (
     <>
       <Carousel>
         <Carousel.Item>
-        <div className="original">
-           </div>
-          {/* src="holder.js/800x400?text=First slide&bg=373940" */}
-          <img src={original}
+          <img
+            className="d-block w-100"
+            src={require("../icon/horizontal.jpg")}
+            alt="Third slide"
           />
+
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
+          {/* src="holder.js/800x400?text=First slide&bg=373940" */}
+
+          
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={require('./carousel01.jpg')}
+            src={require("../icon/horizontal1.jpg")}
             alt="Third slide"
           />
 
@@ -34,7 +38,21 @@ function Banner(props) {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={require('./carousel01.jpg')}
+            src={require("../icon/horizontal2.jpg")}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={require("../icon/horizontal5.jpg")}
             alt="Third slide"
           />
 
@@ -47,7 +65,7 @@ function Banner(props) {
         </Carousel.Item>
       </Carousel>
     </>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
