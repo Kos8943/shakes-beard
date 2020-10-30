8fb8997d1ea105686778f57c943f2b149d67226e
Lin yu <canty38@yahoo.com>
2020-10-30 17:44:06 +0800

    新增按鈕

--- src/zihyu/ChooseRazor.js
+++ src/zihyu/ChooseRazor.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Style/ChooseRazorCSS.css';
+import { Button } from 'react-bootstrap'
 class Class_ChooseRazor extends React.Component
 
 
@@ -18,6 +19,15 @@ class Class_ChooseRazor extends React.Component
                   <div className="Title2">選擇刮鬍刀</div>
                   <div className="lineTop"></div>
                   <div className="lineDown"></div>
+
+                  <a href="/Path_Budget" target="_blank">
+                      <Button className="hayy"> Link Button </Button>
+                  </a>
+
+                  
+                  <a className="bigBtn" href="/Path_Budget"><button className="backButton">上一步</button></a>
+                  <a className="bigBtn" href=""><button className="startButton">下一步</button></a>
+
                   
                 </div>
               </div>
