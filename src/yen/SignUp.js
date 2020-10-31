b86720a08dcdcdd54182ec4ff0886f2583ab7298
Ellie <s95147852@gmail.com>
2020-11-01 04:20:31 +0800

    sign up

---
+++ src/yen/SignUp.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import SignUpCss from './styles/SignUpCss.scss'
+
+
+
+function LogIn(props) {
+   
+    return (
+      <>
+        <div className="bagimg">
+          <div className="bagimg2">
+            
+            <div className="signUpArea">
+
+              <div className="logTittle">
+                註冊新帳號
+              </div>
+
+              <div className="area d-flex flex-column">
+
+                <div className="signUpText">姓名</div>
+                <div className="d-flex justify-content-center">
+                  <input className="signUpInput" type="email"></input>
+                </div>
+
+                <div className="signUpText">信箱</div>
+                <div className="d-flex justify-content-center">
+                  <input className="signUpInput" type="email"></input>
+                </div>
+                <div className="signUpText">手機</div>
+                <div className="d-flex justify-content-center">
+                  <input className="signUpInput" type="email"></input>
+                </div>
+                <div className="signUpText">密碼</div>
+                <div className="d-flex justify-content-center">
+                  <input className="signUpInput" type="email"></input>
+                </div>
+                <div className="signUpText">確認密碼</div>
+                <div className="d-flex justify-content-center">
+                  <input className="signUpInput" type="email"></input>
+                </div>
+
+              </div>
+
+              <div className="buttonArea d-flex justify-content-center">
+                <a href="" className="singUpButton d-flex justify-content-center">完成</a>
+              </div>
+               
+            </div>
+
+          </div>
+        </div>
+        
+       
+
+      </>
+       
+    )
+      
+  }
+
+
+
+export default LogIn
