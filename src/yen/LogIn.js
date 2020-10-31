55395abbc41d2e1a0477b1cd2399dddcb32927ad
Ellie <s95147852@gmail.com>
2020-11-01 05:16:17 +0800

    login 小修改

--- src/yen/LogIn.js
+++ src/yen/LogIn.js
@@ -22,55 +22,52 @@ function LogIn(props) {
 
                   
                     <div className="log">
-                      <div className="logText">信箱</div>
+
+                      <div className="logText d-flex justify-content-around">信箱<div></div><div></div> </div>
                       <div className="account">
                         <input className="logInInput" type="email"></input>
                       </div>
                       
-                      <div className="logText">密碼</div>
+                      <div className="logText d-flex justify-content-around">密碼<div></div><div></div> </div>
                       <div className="password">
                         <input className="logInInput" type="password"></input>
                       </div> 
-                      <div className="forgotPassword">忘記密碼</div>
+                      <div className="d-flex justify-content-around">忘記密碼<div></div><div></div> </div>
+
                     </div>
 
                 
-                    <div className="buttonArea d-flex justify-content-center">
+                    {/* <div className="buttonArea d-flex justify-content-center align-items-center">
                       <a href=""><button className="checkButton">登入</button></a>
                     </div>
-                    
-
-                  
-                  
+                     */}
                   
                 </div>
 
                 <div className="area2">
 
-                  
+                  <div className="otherAccount">
+                    <a href=""><button className="otherLogButton facebook"> <div className="d-flex justify-content-around align-items-center"><img src={facebook} className="lognIcon "></img>使用Facebook登入<div></div></div></button></a>
 
-                    <div className="otherAccount">
-                      <a href=""><button className="otherLogButton facebook"> <div className="d-flex justify-content-around"><img src={facebook} className="lognIcon "></img>使用Facebook登入<div></div></div></button></a>
+                     <a href=""><button className="otherLogButton twitter"><div className="d-flex justify-content-around align-items-center"><img src={twitter} className="lognIcon"></img>使用Twitter登入<div></div></div></button></a>
 
-                      <a href=""><button className="otherLogButton twitter"><div className="d-flex justify-content-around"><img src={twitter} className="lognIcon"></img>使用Twitter登入<div></div></div></button></a>
-
-                      <a href=""><button className="otherLogButton google"><div className="d-flex justify-content-around"><img src={google} className="lognIcon"></img>使用Google登入<div></div></div></button></a>
-                    </div>
+                    <a href=""><button className="otherLogButton google"><div className="d-flex justify-content-around align-items-center"><img src={google} className="lognIcon"></img>使用Google登入<div></div></div></button></a>
+                  </div>
                    
 
-                    <div className="buttonArea d-flex justify-content-center">
-                       <a href=""><button className="singUpButton d-flex justify-content-center">註冊帳號</button></a>
-                    </div>
-                   
-                    
-
-                    
-
-                  
-
-                  
+                    {/* <div className="buttonArea d-flex justify-content-center">
+                       <a href=""><button className="singUpButton d-flex justify-content-center align-items-center">註冊帳號</button></a>
+                    </div> */} 
                 </div>
 
+              </div>
+
+              <div className="d-flex justify-content-around ">
+                  
+                <a href=""><button className="checkButton">登入</button></a>
+                   
+                <a href=""><button className="singUpButton d-flex justify-content-center align-items-center">註冊帳號</button></a>
+                      
               </div>
 
 
