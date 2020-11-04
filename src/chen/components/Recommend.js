47098f5c4a92f2d1072efe801e5cb7e74f6fbd0f
yjchen-iii <yjchen-iii@users.noreply.github.com>
2020-11-04 15:57:19 +0800

    fix miss

--- src/chen/components/Recommend.js
+++ src/chen/components/Recommend.js
@@ -7,54 +7,54 @@ function Recommend(props) {
     <>
           <h4 className="mt-5">推薦給您</h4>
           <div className="d-flex mb-5 scrollRight">
-              <div class="card recommend">
+              <div className="card recommend">
                 <div className="card-img">
                   <img src={require('../img/card01.jpg')} className="card-img-top" alt="..." />
                 </div>
-                <div class="card-body d-flex flex-column align-items-start">
+                <div className="card-body d-flex flex-column align-items-start">
                   <p>
                     <Badge pill variant="secondary">理髮</Badge>
                   </p>
-                  <h6 class="mb-1">Sculptor Barber</h6>
+                  <h6 className="mb-1">Sculptor Barber</h6>
                   <a href="#">查看店家</a>
                 </div>
               </div>
-              <div class="card recommend">
+              <div className="card recommend">
                 <div className="card-img">
                   <img src={require('../img/card01.jpg')} className="card-img-top" alt="..." />
                 </div>
-                <div class="card-body d-flex flex-column align-items-start">
+                <div className="card-body d-flex flex-column align-items-start">
                   <p>
                     <Badge pill variant="secondary">理髮</Badge>
                   </p>
-                  <h6 class="mb-1">Sculptor Barber</h6>
+                  <h6 className="mb-1">Sculptor Barber</h6>
                   <a href="#">查看店家</a>
                 </div>
               </div>
-              <div class="card recommend">
+              <div className="card recommend">
                 <div className="card-img">
                   <img src={require('../img/card01.jpg')} className="card-img-top" alt="..." />
                 </div>
-                <div class="card-body d-flex flex-column align-items-start">
+                <div className="card-body d-flex flex-column align-items-start">
                   <p>
                     <Badge pill variant="secondary">理髮</Badge>
                   </p>
-                  <h6 class="mb-1">
-                    <a class="text-dark" href="#">Sculptor Barber</a>
+                  <h6 className="mb-1">
+                    <a className="text-dark" href="#">Sculptor Barber</a>
                   </h6>
                   <a href="#">查看店家</a>
                 </div>
               </div>
-              <div class="card recommend">
+              <div className="card recommend">
                 <div className="card-img">
                   <img src={require('../img/card01.jpg')} className="card-img-top" alt="..." />
                 </div>
-                <div class="card-body d-flex flex-column align-items-start">
+                <div className="card-body d-flex flex-column align-items-start">
                   <p>
                     <Badge pill variant="secondary">理髮</Badge>
                   </p>
-                  <h6 class="mb-1">
-                    <a class="text-dark" href="#">Sculptor Barber</a>
+                  <h6 className="mb-1">
+                    <a className="text-dark" href="#">Sculptor Barber</a>
                   </h6>
                   <a href="#">查看店家</a>
                 </div>
