320203f500012486ef783da00f5ce765edd65342
Ellie <s95147852@gmail.com>
2020-11-05 21:24:17 +0800

    fix

--- src/yen/MemberOption.js
+++ src/yen/MemberOption.js
@@ -7,186 +7,113 @@ import MemberPreferenceSetting from "./components/MemberPreferenceSetting";
 import MemberFavorites from "./components/MemberFavorites";
 import CartOrderCheck from "../kos/components/CartOrderCheck";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function MemberOption(props) {
+    const [pageTitle, setPageTitle] = useState("訂單查詢");
    
 
-  const [pageTitle, setPageTitle] = useState("訂單查詢");
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row justify-content-between">
+                    <div className="userName d-lg-none">王大明</div>
+                    <div className="decLine  d-lg-none"></div>
+                    <div className="leftArea col-lg-2 col-12 ">
+                        <div className="userName d-none d-lg-block">王大明</div>
+                        <div className="decLine d-none d-lg-block"></div>
 
 
-  return (
-    <>
-      <div className="container-fluid">
-        <div className="row justify-content-between">
-          <div className="userName d-lg-none">王大明</div>
-          <div className="decLine  d-lg-none"></div>
-          <div className="leftArea col-lg-2 col-12 ">
-            <div className="userName d-none d-lg-block">王大明</div>
-            <div className="decLine d-none d-lg-block"></div>
-            
+                        <div className="d-flex flex-column">
+                            <img
+                                src="./imgs/1-3.jpg"
+                                className="PhoneIcon"
+                                onClick={() => { setPageTitle("資料修改"); }}>
 
-            <div className="d-flex flex-column">
-              <img
-                src="./imgs/1-3.jpg"
-                className="PhoneIcon"
-                onClick={() => {
-                  setPageTitle("資料修改");
-                }}
-              ></img>
-              <div
-                className={`option ${
-                  pageTitle === "資料修改" ? "thisPage" : ""
-                }`}
-                onClick={() => {
-                  setPageTitle("資料修改");
-                }}
-              >
-                資料修改
-              </div>
-            </div>
+                            </img>
+                            <div className={`option ${pageTitle === "資料修改" ? "thisPage" : ""}`} onClick={() => { setPageTitle("資料修改"); }}>
+                                資料修改
+                            </div>
+                        </div>
 
-            <div className="d-flex flex-column">
-              <img
-                src="./imgs/1-3.jpg"
-                className="PhoneIcon"
-                onClick={() => {
-                  setPageTitle("密碼修改");
-                }}
-              ></img>
-              <div
-                className={`option ${
-                  pageTitle === "密碼修改" ? "thisPage" : ""
-                }`}
-                onClick={() => {
-                  setPageTitle("密碼修改");
-                }}
-              >
-                密碼修改
-              </div>
-            </div>
+                        <div className="d-flex flex-column">
+                            <img
+                                src="./imgs/1-3.jpg"
+                                className="PhoneIcon"
+                                onClick={() => {
+                                    setPageTitle("密碼修改");
+                                }}>
 
-            <div className="d-flex flex-column">
-              <img
-                src="./imgs/1-3.jpg"
-                className="PhoneIcon"
-                onClick={() => {
-                  setPageTitle("偏好設定");
-                }}
-              ></img>
+                            </img>
+                            <div className={`option ${pageTitle === "密碼修改" ? "thisPage" : ""}`} onClick={() => { setPageTitle("密碼修改"); }}>
+                                密碼修改
+                            </div>
+                        </div>
 
-              <div
-                className={`option ${
-                  pageTitle === "偏好設定" ? "thisPage" : ""
-                }`}
-                onClick={() => {
-                  setPageTitle("偏好設定");
-                }}
-              >
-                偏好設定
-              </div>
-            </div>
+                        <div className="d-flex flex-column">
+                            <img
+                                src="./imgs/1-3.jpg"
+                                className="PhoneIcon"
+                                onClick={() => { setPageTitle("偏好設定"); }}>
 
-                <div className="leftArea col-lg-2 d-flex">
-                    <div className="userName">王大明</div>
-                    <div className="decLine" ></div>
-                    <div className={`option ${(pageTitle === "資料修改") ? "thisPage" : ''}`} onClick={() => { setPageTitle("資料修改") }}>資料修改</div>
-                    <div className={`option ${(pageTitle === "密碼修改") ? "thisPage" : ''}`} onClick={() => { setPageTitle("密碼修改") }}>密碼修改</div>
-                    <div className={`option ${(pageTitle === "偏好設定") ? "thisPage" : ''}`} onClick={() => { setPageTitle("偏好設定") }}>偏好設定</div>
-                    <div className={`option ${(pageTitle === "追蹤清單") ? "thisPage" : ''}`} onClick={() => { setPageTitle("追蹤清單") }}>追蹤清單</div>
-                    <div className={`option ${(pageTitle === "訂單查詢") ? "thisPage" : ''}`} onClick={() => { setPageTitle("訂單查詢") }}>訂單查詢</div>
-                </div>
-            <div className="d-flex flex-column">
-              <img
-                src="./imgs/1-3.jpg"
-                className="PhoneIcon"
-                onClick={() => {
-                  setPageTitle("追蹤清單");
-                }}
-              ></img>
+                            </img>
 
-              <div
-                className={`option ${
-                  pageTitle === "追蹤清單" ? "thisPage" : ""
-                }`}
-                onClick={() => {
-                  setPageTitle("追蹤清單");
-                }}
-              >
-                追蹤清單
-              </div>
-            </div>
+                            <div className={`option ${pageTitle === "偏好設定" ? "thisPage" : ""}`} onClick={() => { setPageTitle("偏好設定"); }}>
+                                偏好設定
+                            </div>
+                        </div>
 
-            <div className="d-flex flex-column">
-              <img
-                src="./imgs/1-3.jpg"
-                className="PhoneIcon"
-                onClick={() => {
-                  setPageTitle("訂單查詢");
-                }}
-              ></img>
+                        <div className="d-flex flex-column">
+                            <img
+                                src="./imgs/1-3.jpg"
+                                className="PhoneIcon"
+                                onClick={() => {
+                                    setPageTitle("追蹤清單");
+                                }}>
 
-              <div
-                className={`option ${
-                  pageTitle === "訂單查詢" ? "thisPage" : ""
-                }`}
-                onClick={() => {
-                  setPageTitle("訂單查詢");
-                }}
-              >
-                訂單查詢
-              </div>
-              
-            </div>
-          </div><div className="decLine  d-lg-none"></div>
+                            </img>
 
-          {/* {(pageTitle === "資料修改") ?
+                            <div className={`option ${pageTitle === "追蹤清單" ? "thisPage" : ""}`} onClick={() => { setPageTitle("追蹤清單"); }}>
+                                追蹤清單
+                            </div>
+                        </div>
+
+                        <div className="d-flex flex-column">
+                            <img
+                                src="./imgs/1-3.jpg"
+                                className="PhoneIcon"
+                                onClick={() => {
+                                    setPageTitle("訂單查詢");
+                                }}>
+                            </img>
+
+                            <div className={`option ${pageTitle === "訂單查詢" ? "thisPage" : ""}`} onClick={() => { setPageTitle("訂單查詢"); }}>
+                                訂單查詢
+                            </div>
+
+                        </div>
+                    </div><div className="decLine  d-lg-none"></div>
+
+                    {/* {(pageTitle === "資料修改") ?
                     <MemberDataUpdateTable title="資料修改" /> : ''}
-
                 {(pageTitle === "密碼修改") ?
                     <MemberPasswordUpdateTable title="密碼修改" /> : ''}
-
                 {(pageTitle === "偏好設定") ?
                     <MemberPreferenceSetting title="偏好設定" /> : ''}
-
                 {(pageTitle === "追蹤清單") ?
                     <MemberFavorites title="追蹤清單" /> : ''}
-
                 {(pageTitle === "訂單查詢") ?
-                    <MemberDataUpdateTable title="訂單查詢" /> : ''}
-
-
-
-
-
-
-
-
-            </div>
                     <CartOrderCheck title="訂單查詢" /> : ''} */}
-        </div>
-      </div>
+                </div>
+            </div>
 
-      {/* <div className="rightArea col-9">
+            {/* <div className="rightArea col-9">
             <div className="optionTittle">資料修改</div>
             <div className="decLine"></div>
             <div className="">
                 {}
             </div>
         </div> */}
-    </>
-  );
+        </>
+    );
 }
 
 export default MemberOption;
