c8762c73eced9f855b4b1428b69b361d0c861723
Ellie <s95147852@gmail.com>
2020-11-05 21:05:02 +0800

    member optionds

--- src/yen/MemberOption.js
+++ src/yen/MemberOption.js
@@ -5,23 +5,36 @@ import MemberPasswordUpdateTable from './components/MemberPasswordUpdateTable'
 import MemberPreferenceSetting from './components/MemberPreferenceSetting'
 import MemberFavorites from './components/MemberFavorites'
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function MemberOption(props) {
     const [pageTitle, setPageTitle] = useState("資料修改")
-    const [titleColor, setTitleColor] = useState("資料修改")
+
 
     return <>
 
         <div className="container-fluid">
             <div className="row justify-content-between">
 
-                <div className="leftArea col-2 d-flex flex-column">
+                <div className="leftArea col-lg-2 d-flex">
                     <div className="userName">王大明</div>
                     <div className="decLine" ></div>
-                    <div className={`option ${(pageTitle === "資料修改") ?"thisPage": ''}`} onClick={() => { setPageTitle("資料修改") }}>資料修改</div>
-                    <div className={`option ${(pageTitle === "密碼修改") ?"thisPage": ''}`} onClick={() => { setPageTitle("密碼修改") }}>密碼修改</div>
-                    <div className={`option ${(pageTitle === "偏好設定") ?"thisPage": ''}`} onClick={() => { setPageTitle("偏好設定") }}>偏好設定</div>
-                    <div className={`option ${(pageTitle === "追蹤清單") ?"thisPage": ''}`} onClick={() => { setPageTitle("追蹤清單") }}>追蹤清單</div>
-                    <div className={`option ${(pageTitle === "訂單查詢") ?"thisPage": ''}`} onClick={() => { setPageTitle("訂單查詢") }}>訂單查詢</div>
+                    <div className={`option ${(pageTitle === "資料修改") ? "thisPage" : ''}`} onClick={() => { setPageTitle("資料修改") }}>資料修改</div>
+                    <div className={`option ${(pageTitle === "密碼修改") ? "thisPage" : ''}`} onClick={() => { setPageTitle("密碼修改") }}>密碼修改</div>
+                    <div className={`option ${(pageTitle === "偏好設定") ? "thisPage" : ''}`} onClick={() => { setPageTitle("偏好設定") }}>偏好設定</div>
+                    <div className={`option ${(pageTitle === "追蹤清單") ? "thisPage" : ''}`} onClick={() => { setPageTitle("追蹤清單") }}>追蹤清單</div>
+                    <div className={`option ${(pageTitle === "訂單查詢") ? "thisPage" : ''}`} onClick={() => { setPageTitle("訂單查詢") }}>訂單查詢</div>
                 </div>
 
                 {(pageTitle === "資料修改") ?
@@ -39,7 +52,7 @@ function MemberOption(props) {
                 {(pageTitle === "訂單查詢") ?
                     <MemberDataUpdateTable title="訂單查詢" /> : ''}
 
-                
+
 
 
 
