5ad1b00496a0d301cd03dda789d0f7566d3535d2
Ellie <s95147852@gmail.com>
2020-11-09 18:28:11 +0800

    mamber資料連接

--- src/yen/LogIn.js
+++ src/yen/LogIn.js
@@ -5,6 +5,9 @@ import twitter from "./img/twitter.svg";
 import google from "./img/google.svg";
 
 function LogIn(props) {
+  const [acc, setAcc] = useState("");
+  const [pass, setPass] = useState("");
+
   return (
     <>
       <div className="bagimg">
@@ -12,22 +15,60 @@ function LogIn(props) {
           <div className="loginCard">
             <div className="loginTittle">登入</div>
 
-            <div className="loginWeb">
+            <form
+              className="loginWeb"
+              method="POST"
+              name="memberForm"
+              onsubmit="return false;"
+              novalidate
+            >
               <div className="loginArea1">
-                <div className="logText">帳號</div>
-                <input className="loginInput" type="email"></input>
-                <div className="logText">密碼</div>
-                <input className="loginInput" type="password"></input>
+                <lable for="account" className="logText">
+                  帳號 </lable>
+                  <input
+                    className="loginInput"
+                    name="account"
+                    type="text"
+                    id="account"
+                    value={acc}
+                    onChange={(e) => setAcc(e.target.value)}
+                    required
+                  ></input>
+               
+
+
+                <label for="password" className="logText">
+                  密碼</label>
+                  <input
+                    className="loginInput"
+                    name="password"
+                    type="password"
+                    id="password"
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    required
+                  ></input>
+                
+
                 <div className="logText2">忘記密碼</div>
-                <a href="">
-                  <button className="loginCheckButton loginMobile">登入</button>
-                </a>
+                <button
+                  type="submit"
+                  className="loginCheckButton loginMobile" >
+                  登入
+                </button>
               </div>
+
+
+
+
+
+
+
+
 
               <div className="loginArea2 ">
                 <div className="otherAccount">
                   <button className="otherLogButton facebook">
-                    {" "}
                     <div className="d-flex justify-content-around align-items-center">
                       <img src={facebook} className="lognIcon "></img>
                       使用Facebook登入<div></div>
@@ -48,26 +89,24 @@ function LogIn(props) {
                     </div>
                   </button>
 
-                  <button className="loginSingUpButton loginMobile">
+                  <button
+                    type="submit"
+                    className="loginSingUpButton loginMobile"
+                  >
                     註冊帳號
                   </button>
                 </div>
               </div>
-            </div>
+            </form>
 
             <div className="d-flex justify-content-around ">
-              <a href="">
-                <button className="loginCheckButton loginWebNone">登入</button>
-              </a>
-
-              <a href="">
-                <button className="loginSingUpButton loginWebNone">
-                  註冊帳號
-                </button>
-              </a>
+              <button type="submit" className="loginCheckButton loginWebNone">
+                登入
+              </button>
+              <button type="submit" className="loginSingUpButton loginWebNone">
+                註冊帳號
+              </button>
             </div>
-
-
 
             <div className="loginMobile"></div>
           </div>
--- src/yen/components/MemberDataUpdateTable.js
+++ src/yen/components/MemberDataUpdateTable.js
@@ -1,52 +1,125 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function MemberDataUpdateTable(props) {
-    return <>
+  const [account, setAccount] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birth, setBirth] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  async function getFromServer() {
+
+   
+
+    const url = "http://localhost:3000/try-mem";
+    const request = new Request(url, {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+    });
+
+    const res = await fetch(request);
+    const memberData = await res.json();
+    console.log(memberData);
+
+//待判斷使用者為誰
+    setAccount(memberData[1].account);
+    setName(memberData[1].name);
+    setEmail(memberData[1].email);
+    setPhone(memberData[1].phone);
+    setAddress(memberData[1].address);
+    setName(memberData[1].name);
+  }
+  useEffect(() => {
+    getFromServer()
+  }, [])
+
+    return (
+      <>
         <div className="rightArea col-9">
-            <div className="optionTittle">{props.title}</div>
-            <div className="decLine"></div>
-            <div className="updateArea">
-                <div>
-                    <div>會員帳號</div>
-                    <input type="text" disabled="disabled" className="updateInput" value="xxx" ></input>
-                    <div>姓名</div>
-                    <input type="text" className="updateInput" value="王大明"></input>
-                    <div>信箱</div>
-                    <input type="text" className="updateInput" value="xxx@xxx.com"></input>
-                    <div>生日</div>
-                    <div className="d-flex">
-                        <select className="birthday">
-                            <option>1</option>
-                        </select>
-                        <div className="birthdayText">月</div>
-                        <select className="birthday">
-                            <option>13</option>
-                        </select>
-                        <div className="birthdayText">日</div>
-                    </div>
-                    {/* <input type="text" className="updateInput" placeholder="YYYY-MM-DD"></input> */}
-                    <div>手機</div>
-                    <input type="text" className="updateInput"></input>
-                    <div>地址</div>
-                    <div className="d-flex">
-                        <select>
-                            <option>台北市</option>
-                        </select>
-                        <select>
-                            <option>大安區</option>
-                        </select>
-                    </div>
-                    <input type="text" className="updateInput"></input>
-                </div>
-                <button className="memberCheck">完成</button>
-                <button className="memberCancle">取消</button>
-            </div>
+          <div className="optionTittle">{props.title}</div>
+          <div className="decLine"></div>
+          <div className="updateArea">
+            <form>
+              <div>會員帳號</div>
+              <input
+                type="text"
+                disabled="disabled"
+                className="updateInput"
+                name="account"
+                value={account}
+              ></input>
+              <div>姓名</div>
+              <input
+                type="text"
+                className="updateInput"
+                name="name"
+                value={name}
+                onChange={(e)=>
+                {setName(e.target.value)}}
+              ></input>
+              <div>信箱</div>
+              <input
+                type="text"
+                className="updateInput"
+                name="email"
+                value={email}
+                onChange={(e)=>
+                {setEmail(e.target.value)}}
+              ></input>
+              <div>生日</div>
+              <div className="d-flex">
+                <select className="birthday">
+                  <option>1</option>
+                </select>
+                <div className="birthdayText">月</div>
+                <select className="birthday">
+                  <option>13</option>
+                </select>
+                <div className="birthdayText">日</div>
+              </div>
+              {/* <input type="text" className="updateInput" placeholder="YYYY-MM-DD"></input> */}
+              <div>手機</div>
+              <input
+                type="text"
+                className="updateInput"
+                name="phone"
+                value={phone}
+                onChange={(e)=>
+                {setPhone(e.target.value)}}
+              ></input>
+              <div>地址</div>
+              <div className="d-flex">
+                <select>
+                  <option>台北市</option>
+                </select>
+                <select>
+                  <option>大安區</option>
+                </select>
+              </div>
+              <input
+                type="text"
+                className="updateInput"
+                name="address"
+                value={address}
+                onChange={(e)=>
+                {setAddress(e.target.value)}}
+              ></input>
+            </form>
+            <button className="memberCheck">完成</button>
+            <button className="memberCancle">取消</button>
+          </div>
         </div>
+        {/* <div>{name}</div>
+        <div>{phone}</div> */} 
+        {/* 測試用 */}
+       
+      </>
+    );
+  }
 
-    </>
-}
 
-export default MemberDataUpdateTable
-
-
-
+export default MemberDataUpdateTable;
