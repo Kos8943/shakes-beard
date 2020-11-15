c05ac77f0dbdea466c5962bc6fd9cf443ff0d9c7
Kos8943 <a26095998@gmail.com>
2020-11-15 17:19:23 +0800

    card css修改

--- src/kos/components/CartCardMap.js
+++ src/kos/components/CartCardMap.js
@@ -81,7 +81,7 @@ sum(myCart)
     <>
       {myCart.map((v, i) => (
         <>
-          <div className="cartItem d-xl-flex d-block" key={v.id}>
+          <div className="cartItem d-xl-flex justify-content-between d-block " key={v.id}>
             <img className="itemImg" src={"./imgs/hsuan/" + `${v.img}`}></img>
             <div className="itemName my-lg-auto">{v.name}</div>
 
